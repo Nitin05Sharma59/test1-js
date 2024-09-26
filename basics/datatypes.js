2a9f0e70e4dff8365ac04853c88238db51eda054
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-09-26 15:32:48 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //we will use the "use strict" to treat all the JS code as newer version 
+
+//alert("Hello!") we will face issue in 'alert' because we are using nodejs
+
+let name = "Nitin"
+let age = 21
+let isLoggedIn = true 
+let state = null
+
+//data types 
+/*
+number => 2pow53
+bigint 
+string => "Value stored in double or single quotes"
+boolean => true/false
+null => standalone value => null is an object
+undefined => value is not declared in the variable
+symbol => unique
+*/
+
+//object
+
+console.log(typeof "Nitin")
+console.log(typeof age)
+console.log(typeof null) //null is an object 
+console.log(typeof undefined)
