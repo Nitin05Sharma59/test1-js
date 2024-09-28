2363ae712e15ba879556a975f6191d8703542a7c
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-09-28 15:40:10 +0000

    conversion of datatype

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let id = 22
+
+console.log(typeof id);
+
+let value = "20" 
+console.log(typeof value); // this will print string as value is being declared in double quotes
+console.log(value)
+
+let valueInNumber = Number(value) //by using Number we are changing the datatype of value 
+console.log(typeof valueInNumber) //will print datatype as number 
+console.log(valueInNumber)
+
+/*
+"20" => 20
+"abc" => NaN
+true => 1 false =>0
+*/
+
+let isLoggedIn = 1 // will give fale on 0 and true on any integer
+// "" => value will be false
+// "abc" => value will be true 
+let booleanIsLogggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLogggedIn)
+
