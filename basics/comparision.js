e061204e5074f0b7107767614be2350b53d49056
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-09-28 16:16:47 +0000

    comparision

---
+++ basics/comparision.js
@@ -0,0 +1,20 @@
+/*console.log(2 > 1);
+console.log( 2>= 1);
+console.log(2 < 1)
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+*/
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null >0)//false
+console.log(null<0);//false
+console.log(null==0);//false
+console.log(null>=0)//true null typecasted as 0
+console.log(null<=0);//true 
+
+// === this is used for strict check 
+console.log("2" === 2) //return false because === is used strict checking and it also checks the datatype of both the variables
+
