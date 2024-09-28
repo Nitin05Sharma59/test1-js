ed4dee9dbc545ee7507b82c869380a7d95da1c47
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-09-28 16:17:20 +0000

    operation

---
+++ basics/operations.js
@@ -0,0 +1,26 @@
+let value = 5
+let negval = -value
+console.log(negval)
+
+/*
+//basic maths operations
+console.log(2+2)
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(3%2);
+*/
+
+console.log("1"+2) //will give 12 because 1 is given in the form of string and js converts 2 into string too 
+console.log(1 + "2"); //will give 12 because 2 is given in the form of string and js converts 1 into string too 
+
+console.log("1"+2+2); // will give 122
+console.log(1+2+"2") //will give 32
+console.log(1+"2"+2); //will give 122
+console.log(1+2+3+"5"+2); //will give 652
+// if first value is string then all the next values will converted in string 
+// after any string value all the values will be converted in the string 
+
+
+
