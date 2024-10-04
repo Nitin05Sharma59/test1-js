113db1ca8023787e31be9d4d2b67c900acf446d9
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-10-04 10:34:13 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,38 +1,38 @@
-// //declaring a function
-// // function myName(){
-// //     console.log("N");
-// //     console.log("I");
-// //     console.log("T");
-// //     console.log("I");
-// //     console.log("N");
-// // }
-// // myName()
-
-// function add(num1,num2){
-//     console.log(num1+num2);   
+//declaring a function
+// function myName(){
+//     console.log("N");
+//     console.log("I");
+//     console.log("T");
+//     console.log("I");
+//     console.log("N");
 // }
-// add(5,6)//giving argument 
-// add(6,"4")
-// add(4,"a")
-// add(3,null)
+// myName()
 
-// // arguments => values which are passed as when function is called is known as arguments 
-// //parameter => at the time of defining function the values which are passed in it is called parameter
+function add(num1,num2){
+    console.log(num1+num2);   
+}
+add(5,6)//giving argument 
+add(6,"4")
+add(4,"a")
+add(3,null)
 
-// //we can store the value of function 
+// arguments => values which are passed as when function is called is known as arguments 
+//parameter => at the time of defining function the values which are passed in it is called parameter
 
-// function addTwoNumber(number1,number2){
-//     result = number1+number2
-//     return result
-//     //return number1+number2 //we can also do that 
-// }
+//we can store the value of function 
 
-// console.log(addTwoNumber(5,44));
+function addTwoNumber(number1,number2){
+    result = number1+number2
+    return result
+    //return number1+number2 //we can also do that 
+}
 
-// function login(username){
-//     return `${username} just logged in`
-// }
-// console.log(login("Nitin"));// is no argument is passed then it will be undefined 
+console.log(addTwoNumber(5,44));
+
+function login(username){
+    return `${username} just logged in`
+}
+console.log(login("Nitin"));// is no argument is passed then it will be undefined 
 
 
 //passing object in a function 
