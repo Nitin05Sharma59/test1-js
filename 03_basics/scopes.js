f4bb37f39b693ad2b59e28cf2a7a4ea6e97ddf8b
Nitin05Sharma59 <nitinsharma5948@gmail.com>
2024-10-04 11:01:23 +0000

    scopes

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -1,39 +1,39 @@
-// let a = 200
-// if(true){
-//     let a =1
-//     const b = 5
-//     var c =4
-//     console.log("Inner:",a)
-// }
+let a = 200
+if(true){
+    let a =1
+    const b = 5
+    var c =4
+    console.log("Inner:",a)
+}
  
-// //console.log(a);//will give error as a declared is in local scope 
-// //console.log(b);//will give error as a declared is in local scope 
-// console.log(c); //will print the value of c and this will occur a problem of scope 
+//console.log(a);//will give error as a declared is in local scope 
+//console.log(b);//will give error as a declared is in local scope 
+console.log(c); //will print the value of c and this will occur a problem of scope 
 
-// //if we declare value of a in global scope then it will print both local and global declared value of a
-// console.log(a);
+//if we declare value of a in global scope then it will print both local and global declared value of a
+console.log(a);
 
 
-//nested scope 
-// function abc(){
-//     const name="Nitin"
+nested scope 
+function abc(){
+    const name="Nitin"
 
-//     function two(){
-//         const web = "Yt"
-//         console.log(name); 
-//     }
-//     //console.log(website); cannot be accesed because it is declared in local function two 
-//     two() 
-// }
-// abc()
+    function two(){
+        const web = "Yt"
+        console.log(name); 
+    }
+    //console.log(website); cannot be accesed because it is declared in local function two 
+    two() 
+}
+abc()
 
-// if(true){
-//     const username = "nitin"
-// if(username === "nitin"){
-//     const website ="Yt"
-//     console.log(username + website);   
-//     }
-// }
+if(true){
+    const username = "nitin"
+if(username === "nitin"){
+    const website ="Yt"
+    console.log(username + website);   
+    }
+}
 
 function addone(num){
     return num+1
